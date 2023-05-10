8bc09baf331435cc3520b35be339317a7a879694
tranbon@bacoor.co <tranbon@bacoor.co>
2023-05-10 13:43:23 +0700

    Update learn canvas

---
+++ algorithm/TwoSum/index.js
@@ -0,0 +1,13 @@
+
+function twoSum(arrays, target) {
+    for (let i = 0; i < arrays.length; i++) {
+        const compliment = target - arrays[i]
+        console.log("compliment", compliment)
+        // if(arrays.findIndex((item) => ))
+    }
+}
+
+
+const arr = [1, 3, 5, 6]
+
+const result = twoSum(arr, 8)
